cba6d19845ddc17876d7d402c15fa18af5975083
momoessop <moessop@outlook.com>
2024-07-23 12:48:27 +0200

    part 1 work

---
+++ mooc-java-programming-i/part01-Part01_07.MessageThreeTimes/src/main/java/MessageThreeTimes.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class MessageThreeTimes {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write a message:");
+        // Write your program here
+
+        String lines = scanner.nextLine();
+        System.out.println(lines);
+        System.out.println(lines);
+        System.out.println(lines);
+
+    }
+}
